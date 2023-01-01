3a275f2fcf3582e818786b69cf94adddbee41d93
Martin Uzunov <96121194+MartinUzunuv@users.noreply.github.com>
2023-01-01 20:05:13 +0200

    V1.3

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ arr = [];
 
 maxdist = 30;
 
+distForAll = 15
+
 function dist(x1, x2, y1, y2) {
   return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 }
@@ -33,12 +35,35 @@ class point {
         sused.y += Math.sin(a);
       } while (dist(this.x, sused.x, this.y, sused.y) > maxdist);
     } else if (dist(this.x, sused.x, this.y, sused.y) < maxdist - 1) {
-      let a = Math.atan2(this.y - sused.y, this.x - sused.x, 2);
+      let a = Math.atan2(this.y - sused.y, this.x - sused.x);
       do {
         sused.x -= Math.cos(a);
         sused.y -= Math.sin(a);
       } while (dist(this.x, sused.x, this.y, sused.y) < maxdist);
     }
+  }
+  // defaultGet(sused) {
+  //   if (dist(this.x, sused.x, this.y, sused.y) > maxdist + 1) {
+  //       let a = Math.atan2(this.y - sused.y, this.x - sused.x, 2);
+  //       sused.x += Math.cos(a);
+  //       sused.y += Math.sin(a);
+  //     } else if (dist(this.x, sused.x, this.y, sused.y) < maxdist - 1) {
+  //       let a = Math.atan2(this.y - sused.y, this.x - sused.x);
+  //       sused.x -= Math.cos(a);
+  //       sused.y -= Math.sin(a);
+  //     }
+  // }
+  update(){
+    arr.forEach(p => {
+      if(dist(this.x, p.x, this.y, p.y) < distForAll){
+        let a = Math.atan2(this.y - p.y, this.x - p.x)
+        this.x += Math.cos(a);
+        this.y += Math.sin(a);
+      }
+    })
+    // this.susedi.forEach(p => {
+    //   this.defaultGet(arr[p]);
+    // });
   }
   getsusedi() {
     // console.log(this.susedi)
@@ -58,12 +83,33 @@ class point {
 // arr.push(new point(300,300,[2,4]))
 // arr.push(new point(400,400,[3]))
 
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j < 10; j++) {
-    arr.push(new point(50 * j, 50 * i, [j + 1 + 11 * i]));
+
+let linelen = 20
+for(let j =0; j < linelen; j++){
+for(let i = 0; i < 20; i++){
+  if(i == 0){
+    arr.push(new point(20 + i * 30, 20 + j* 30,[1 + (linelen) *j]))
+  }else if(i == linelen - 1){
+    arr.push(new point(20 + i * 30, 20 + j* 30,[linelen - 2 + (linelen) *j]))
+  }else{
+    arr.push(new point(20 + i * 30, 20 + j* 30,[i+1 + (linelen) *j, i -1 + (linelen) *j]))
   }
-  arr.push(new point(50 * 11, 50 * i, []));
 }
+// if(j != linelen - 1){
+//   arr[arr.length - 1].susedi.push(linelen*(j+1))
+// }
+}
+
+// arr.push(new point(100,100,[1,2]))
+// arr.push(new point(100,100,[0,2]))
+// arr.push(new point(100,100,[1,0]))
+
+// for (let i = 0; i < 10; i++) {
+//   for (let j = 0; j < 10; j++) {
+//     arr.push(new point(50 * j, 50 * i, [j + 1 + 11 * i]));
+//   }
+//   arr.push(new point(50 * 11, 50 * i, []));
+// }
 
 // arr.push(new point(100,100,[1,3]))    //0           0 1 2
 // arr.push(new point(150,100,[0,4,2]))  //1           3 4 5
@@ -77,6 +123,14 @@ for (let i = 0; i < 10; i++) {
 // arr.push(new point(150,200,[6,4,8]))  //7
 // arr.push(new point(200,200,[7,5]))  //8
 
+// arr.push(new point(300, 300, [1]))
+// arr.push(new point(300, 350, [0,2,5,6]))
+// arr.push(new point(300, 400, [1,3,4]))
+// arr.push(new point(350, 400, [2]))
+// arr.push(new point(250, 400, [2]))
+// arr.push(new point(350, 350, [1]))
+// arr.push(new point(250, 350, [1]))
+
 function draw() {
   arr.forEach((p) => {
     p.draw();
@@ -86,9 +140,10 @@ function draw() {
 arr[0].getsusedi();
 
 function update() {
-  // arr.forEach(p => {
-  //     p.getsusedi()
-  // });
+  arr.forEach(p => {
+      p.update()
+      p.x--
+  });
   arr.forEach((p) => {
     p.move = false;
   });
